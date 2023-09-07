022c1c99c97457df148aeaf5564ea1263f833cfb
pradeep <pradeepvirat96@gmail.com>
2023-09-07 14:06:19 +0530

    Adding Comments

--- 404.php
+++ 404.php
@@ -1,30 +1,29 @@
 <?php 
-
-    if(!isset($_SERVER['HTTP_REFERER'])){
-        // redirect them to your desired location
+    // Check if the HTTP_REFERER header is not set (referrer is not present).
+    if (!isset($_SERVER['HTTP_REFERER'])) {
+        // Redirect the user to a specific location (http://localhost/freshcery/index.php).
         header('location: http://localhost/freshcery/index.php');
-        exit;
+        exit; // Terminate script execution after the redirection.
     }
-
 ?>
 
-<?php require "includes/header.php"; ?>
-<?php require "config/config.php"; ?>
+<?php require "includes/header.php"; // Include the header.php file. ?>
+<?php require "config/config.php"; // Include the config.php file. ?>
 
 <div class="banner">
-            <div class="jumbotron jumbotron-bg text-center rounded-0" style="background-image: url('<?php echo APPURL; ?>/assets/img/bg-header.jpg');">
-                <div class="container">
-                    <h1 class="pt-5">
-                        404 page, we cannot find the page that you are looking for.
-                    </h1>
-                    <!-- <p class="lead">
-                        You can check your orders now.
-                    </p> -->
-                    <a href="<?php echo APPURL; ?>" class="btn btn-primary text-uppercase">home</a>
-
-                  
-                </div>
-            </div>
+    <div class="jumbotron jumbotron-bg text-center rounded-0" style="background-image: url('<?php echo APPURL; ?>/assets/img/bg-header.jpg');">
+        <div class="container">
+            <h1 class="pt-5">
+                404 page, we cannot find the page that you are looking for.
+            </h1>
+            <!--
+            <p class="lead">
+                You can check your orders now.
+            </p>
+            -->
+            <a href="<?php echo APPURL; ?>" class="btn btn-primary text-uppercase">home</a>
+        </div>
+    </div>
 </div>
 
-<?php require "includes/footer.php"; ?>
+<?php require "includes/footer.php"; // Include the footer.php file. ?>
